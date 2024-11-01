b0c597a9562678a7691fee1ec7b1dcc8c494aa5c
Neo Su <uninet.neo@gmail.com>
2024-11-01 09:14:41 +0800

    Improve soring logic for GetSpritesFromTexture in Utilities.cs

--- Editor/Scripts/Utilities.cs
+++ Editor/Scripts/Utilities.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor;
-using UnityEditor.UIElements;
 
 namespace QuickRuleTileEditor
 {
@@ -14,14 +13,15 @@ namespace QuickRuleTileEditor
             var path = AssetDatabase.GetAssetPath(texture);
             Object[] subAssets = AssetDatabase.LoadAllAssetsAtPath(path);
 
-            //Sort ths Sprite by their name's trail number.
-            subAssets = subAssets.OrderBy(x => {
-                string indexStr = x.name.Substring(x.name.LastIndexOf('_') + 1);
+            //Sort the Sprite by their name's trailing number.
+            subAssets = subAssets.OrderBy(x =>
+            {
+                string trailingNumber = string.Concat(x.name.ToArray().Reverse().TakeWhile(char.IsNumber).Reverse());
                 int index = 0;
-                int.TryParse(indexStr, out index);
+                int.TryParse(trailingNumber, out index);
                 return index;
             }).ToArray();
-            
+
             return subAssets.OfType<Sprite>();
         }
     }
